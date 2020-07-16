077cc11c5f0ea7f6a1e340b00a43ee50347b6005
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-16 16:15:15 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -11,10 +11,17 @@ class Book
   def author
     @author
   end
-  def page_count(n)
+  def page_count=(n)
     @page_count = n
   end
   def page_count
     @page_count
   end
+  def genre=(b)
+    @genre = b
+  end
+  def genre
+    @genre
+  end
+  
 end
