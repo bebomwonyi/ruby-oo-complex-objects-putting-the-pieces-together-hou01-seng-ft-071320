af7b1d1fac5d93949ebc148d85ed11469a3c9a2f
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-16 16:20:13 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,27 +2,9 @@ class Book
   def initialize(title)
     @title = title
   end
-  attr_accessor :
+  attr_accessor :author :page_count :genre
   def title
     @title
-  end
-  def author=(a)
-    @author = a
-  end
-  def author
-    @author
-  end
-  def page_count=(n)
-    @page_count = n
-  end
-  def page_count
-    @page_count
-  end
-  def genre=(b)
-    @genre = b
-  end
-  def genre
-    @genre
   end
   def turn_page
     puts "Flipping the page...wow, you read fast!"
